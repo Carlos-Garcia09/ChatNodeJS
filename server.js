b4fabac6d03815764e2ca5af41e4672d0a9850e9
Carlos-Garcia09 <Carlos.GMadrigal@alumno.udg.mx>
2023-09-07 17:50:13 -0600

    Chat terminado

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log('Usuario conectado');
+
+  socket.on('chat message', (msg, username) => {
+    io.emit('chat message', { message: msg, user: username });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
